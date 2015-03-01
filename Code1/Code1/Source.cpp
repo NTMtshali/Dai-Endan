653af7260eb28c323f756857d93789e94322fb6c
NTMtshali <213522460@stu.ukzn.ac.za>
2015-03-01 14:46:14 +0200

    commit solution

---
+++ Code1/Code1/Source.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string name;
+
+	cout << "Enter your name: ";
+	cin >> name;
+	cout << "Welcome to C++programing " << name << endl;
+
+	return 0;
+
+}
